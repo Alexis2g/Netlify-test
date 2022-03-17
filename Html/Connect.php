996a96e1f90a0254b319f6040a83d8c06a35a494
Alexis2g <vuaille.alexis@gmail.com>
2022-03-17 21:15:31 +0100

    ok

--- Html/Connect.php
+++ Html/Connect.php
@@ -1,8 +1,15 @@
+<html>
+    <head>  
+        <title> Contact</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="Technos" >
+        <link href="Style.css" rel="stylesheet" >
+    </head>
+
+    <body>
+        
 
 
-<pre>
-  <?php print_r($_SERVER); ?>
-</pre>
 
 <?php
 $servername = "localhost";
@@ -26,9 +33,49 @@ try {
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-
-
+/*
+<pre>
+  <?php print_r($_SERVER); ?>
+</pre>
+*/
 ?>
 
+<header>
+            Me contacter
+        </header>
+        <article> 
+            <p>Le formulaire pour me contacter par mail</p>
+        </article>
+        <form action="Connect.php">
+            <fieldset>
+                <legend>Le formulaire</legend>
+                <div>
+                    <label for="nominput">Nom</label>
+                    <input type="text" name="nom" id="nominput" placeholder="CHIRAC">
+                </div>
+                <br>
+                <div>
+                    <label for="prenominput">Prénom</label>
+                    <input type="text" name="prenom" id="prenominput" placeholder="Jacques">
+                </div>
+                <br>
+                <div>
+                    <label for="prenominput">Mail</label>
+                    <input type="text" name="mail" id="mailinput" placeholder="mail@azerty.com">
+                </div>
+                <br>
+                <div>
+                    <label for="messageinput">Message</label>
+                    <textarea type="text" cols="30" rows="10" name="message" id="messageinput" placeholder="Wow ce site est incroyable ..."></textarea>
+                </div>
+                <br>
+                <input type="submit" value="Envoyer">                   
+            </fieldset>
+           </form>
+    </body>
+    <footer>
+        <a href="Landing.html">&#8592; Retour à l'accueil</a>
+    </footer>
+</html>
 
 
