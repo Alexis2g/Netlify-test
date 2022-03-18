61a8011662577b135fabd802104d2ac0e9f0cd19
Alexis2g <vuaille.alexis@gmail.com>
2022-03-18 19:41:42 +0100

    testmail

---
+++ Html/TestMail.php
@@ -0,0 +1,15 @@
+<?php
+
+
+   // $to = 'vuaille.alexis@gmail.com',
+    //$subject = 'testing1',
+    //$message = 'hello',
+    
+    
+    mail('vuaille.alexis@gmail.com', 'subject', 'testttt');
+
+
+
+/*
+array|string $additional_headers = [],
+    string $additional_params = ""
