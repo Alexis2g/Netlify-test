fa7d84a143d548101e9ae0dd3a5a7d402d8b16d2
Alexis2g <vuaille.alexis@gmail.com>
2022-03-21 21:39:26 +0100

    hard

--- Html/TestMail.php
+++ Html/TestMail.php
@@ -1,11 +1,15 @@
 <?php
 
+$headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
 
    // $to = 'vuaille.alexis@gmail.com',
     //$subject = 'testing1',
     //$message = 'hello',
     
     
+    
     mail('vuaille.alexis@gmail.com', 'subject', 'testttt', 'From: vuaille.alexis@gmail.com');
 
+    ini_set("SMTP","ssl://smtp.gmail.com");
+    ini_set("smtp_port","465");
     ?>
